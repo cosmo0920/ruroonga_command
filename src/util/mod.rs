94a273b7a40cfa88f66485faf99fc22d7bbe04c2
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-03-05 01:24:42 +0900

    util: Add new util method to treat FlagType vector

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,15 @@
+use types::flag_type::FlagType;
+
 #[inline]
 pub fn split_values_vec(values: Vec<String>) -> String {
     let string = values.into_iter().collect::<Vec<String>>()
         .join(",").to_owned();
+    string.clone()
+}
+
+#[inline]
+pub fn split_flags_vec(values: Vec<FlagType>) -> String {
+    let string = values.into_iter().map(|v| v.as_ref().to_string())
+        .collect::<Vec<String>>().join("|").to_owned();
     string.clone()
 }
