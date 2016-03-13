6ff7a236c6e3188253320cb3e4b20b69c4035160
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-03-13 23:17:54 +0900

    drilldown_builder: Fix document

--- src/selectable/drilldown_builder.rs
+++ src/selectable/drilldown_builder.rs
@@ -27,7 +27,7 @@ pub struct DrilldownBuilder {
 ///     ruroonga::selectable::drilldown_builder::DrilldownBuilder::new(select, drilldown);
 /// let _ = builder.build();
 /// ```
-/// In more practical example, please refer to drilldown_builder_xxx
+/// In more practical example, please refer to drilldown_builder examples
 /// in [examples/drilldown_builder.rs]
 /// (https://github.com/cosmo0920/ruroonga_command/blob/master/examples/drilldown_builder.rs).
 impl DrilldownBuilder {
