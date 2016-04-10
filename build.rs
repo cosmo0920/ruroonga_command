743509b481d6358b270641f864d5b91a38919ea6
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-04-11 08:37:47 +0900

    Make to be optional skeptic dependency

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
+#[cfg(feature="readme-testing")]
 extern crate skeptic;
 
+#[cfg(feature="readme-testing")]
 fn readme_testing() {
     skeptic::generate_doc_tests(&["README.md"]);
+}
+
+#[cfg(not(feature="readme-testing"))]
+fn readme_testing() {
 }
 
 fn main() {
--- tests/skeptic.rs
+++ tests/skeptic.rs
@@ -1,1 +1,2 @@
+#[cfg(feature="readme-testing")]
 include!(concat!(env!("OUT_DIR"), "/skeptic-tests.rs"));
