727ad51b15fed5d1dd6ed3622c0fac5e183c1742
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-04-23 14:39:46 +0900

    Update documentation

--- implementors/ruroonga_command/extendable/trait.Extendable.js
+++ implementors/ruroonga_command/extendable/trait.Extendable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['ruroonga_command'] = [];
+implementors['ruroonga_command'] = [];implementors['ruroonga_command'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
