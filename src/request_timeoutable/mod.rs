a55f4863b08f9f66eec614dcad5ef5bbcb4c545c
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-04-28 22:15:57 +0900

    request_timeoutable: Use 64bit float type to represent under 1 second value

--- src/request_timeoutable/mod.rs
+++ src/request_timeoutable/mod.rs
@@ -1,7 +1,10 @@
 pub trait RequestTimeoutable {
     type Output;
 
-    fn append_request_timeout(self, timeout: u64) -> Self::Output;
+    /// Specify `timeout` value. Unit: second
+    ///
+    /// Panics if specified negative float value.
+    fn append_request_timeout(self, timeout: f64) -> Self::Output;
 }
 
 macro_rules! request_timeoutable {
@@ -9,7 +12,10 @@ macro_rules! request_timeoutable {
         $(impl RequestTimeoutable for $t {
             type Output = $t;
 
-            fn append_request_timeout(mut self, timeout: u64) -> Self::Output {
+            fn append_request_timeout(mut self, timeout: f64) -> Self::Output {
+                if !timeout.is_sign_positive() {
+                    panic!("`timeout` should be positive 64bit float number.")
+                }
                 let string = format!("{}", timeout);
                 self.arguments.insert("request_timeout".to_string(), string.clone());
                 self
