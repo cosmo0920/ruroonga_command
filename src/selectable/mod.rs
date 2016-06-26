6452c85ddc45d91ff526bd53e21222b9b13c7655
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-06-26 13:24:47 +0900

    selectable: Update rustdoc

--- src/selectable/mod.rs
+++ src/selectable/mod.rs
@@ -1,6 +1,8 @@
 //! Provides drilldown related procedures.
 //! This module will be used with `SelectCommand` type by `+` operation
 //! to concatinate `select` and `drilldown` queries.
+//!
+//! Also this module support `pseudo_table` and `staged_columns` manupilation.
 pub mod drilldown;
 pub mod labeled_drilldown;
 pub mod drilldownable;
@@ -9,6 +11,9 @@ pub mod labeled_drilldown_sequencable;
 pub mod drilldown_builder;
 pub mod labeled_drilldown_builder;
 pub mod labeled_drilldown_sequence_builder;
+/// `fragmentable` module provides making query fragment operation.
+/// It is useful to make composable query elements.
+/// It makes `select` query to be composable against drilldown related query and `staged_columns` query.
 pub mod fragmentable;
 pub mod pseudo_table;
 pub mod staged_columns;
